5daeff06c071c58695b7e79ffbff3034eecc69ec
imd <kzn.2003valery@gmail.com>
2020-04-16 19:40:04 +0300

    The end2

--- records/views.py
+++ records/views.py
@@ -18,7 +18,6 @@ def main(request):
     posts_list = Post.objects.all()
     posts = []
     for p in posts_list:
-        print(len(posts_list)-p.id + 1)
         posts.append(Post.objects.get(pk=len(posts_list)-p.id+1))
     s_list = []
     for p in posts_list:
@@ -26,10 +25,6 @@ def main(request):
         s = a.text
         s = s[:444] + '...'
         s_list.append(s)
-        print(s)
-    for p in posts:
-        print(p.title)
-    print(type(posts_list))
     data = zip(posts, s_list)
     context = {'data': data, 'author': author, 'posts_list': posts_list}
     return render(request, 'records/main.html', context)
@@ -39,23 +34,15 @@ def signup(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            print('HEY')
             username = form.cleaned_data.get('username')
-            # my_password = form.cleaned_data.get('password1')
-            # email = form.cleaned_data.get('email')
-            # user = User.objects.create_user(username=username,
-            #                                 email=email,
-            #                                 password=my_password)
             user = form.save()
-            u_id = request.user.id
             login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            # user = User.objects.get(pk=u_id)
-            print('fuck', u_id)
-
             profile = Profile(user=user, nickname=username, count_posts=0, about=' ')
             profile.save()
         else:
-            print("FUCK")
+            flag = True
+            form = RegistrationForm()
+            return render(request, 'registration/signup.html', {'flag': flag, 'form': form})
         return redirect('/')
     else:
         form = RegistrationForm()
@@ -64,12 +51,13 @@ def signup(request):
 
 def p_details(request, post_id):
     post = Post.objects.get(pk=post_id)
-    print(post)
     profile_id = post.profile_id.id
-    print(profile_id)
     author = get_object_or_404(Profile, pk=profile_id)
-    print(author.id)
-    return render(request, 'records/post_details.html', {'post': post, 'author': author})
+    if profile_id == request.user.id:
+        flag = True
+    else:
+        flag = False
+    return render(request, 'records/post_details.html', {'post': post, 'author': author, 'flag': flag})
 
 
 def new_post(request):
@@ -79,23 +67,19 @@ def new_post(request):
             form.save()
         title = form.cleaned_data.get('title')
         text = form.cleaned_data.get('text')
-        # theme = form.cleaned_data.get('theme')
         theme = "test"
         pub_date = timezone.now()
         profile = Profile.objects.get(pk=request.user.id)
         profile.count_posts = profile.count_posts + 1
         profile.save()
-        profile_id = request.user.id
         author = profile.nickname
         likes = 0
-        print(title, text, author, pub_date, profile_id, likes, theme)
         profile.post_set.create(title=title, text=text, theme=theme, pub_date=pub_date, author=author, likes=likes)
         post = Post.objects.all()
         length = len(post)
         return redirect('/posts/' + str(length))
     else:
         form = NewPostForm()
-        print("jjj")
         return render(request, 'records/new_post.html', {'form': form})
 
 
@@ -106,18 +90,14 @@ def u_details(request, username):
     else:
         flag = False
     user_id = int(username)
-    print('user_id: ', user_id)
     s_list = []
     posts_list = Post.objects.filter(profile_id=user_id)
     for p in posts_list:
-        print('len post_list: ', len(posts_list))
-        print('id: ', p.id)
-        print('len: ', len(posts_list)-p.id+1)
-        # a = Post.objects.get(pk=len(posts_list)-p.id+1) TODO: проверь эту большую фиговину
         s = p.text
         s = s[:444] + '...'
         s_list.append(s)
     info = Profile.objects.get(pk=user_id)
+    print(info.about)
     data = zip(posts_list, s_list)
     return render(request, 'records/author.html', {'info': info, 'flag': flag, 'user_id': user_id, 'data': data,
                   'posts_list': posts_list})
@@ -127,20 +107,20 @@ def edit(request):
     info = Profile.objects.get(pk=request.user.id)
     print(request.method)
     if request.method == 'POST':
-        print("HELLO FRIEND")
         form = AuthorForm(request.POST)
         if form.is_valid():
             form.save()
         nickname = form.cleaned_data.get('nickname')
         about = form.cleaned_data.get('about')
         profile = Profile.objects.get(pk=request.user.id)
+        user = User.objects.get(pk=request.user.id)
+        user.username = nickname
+        user.save()
         profile.nickname = nickname
         profile.about = about
-        print(about, profile)
         profile.save()
         flag = True
         return render(request, 'records/author.html', {'flag': flag, 'info': profile})
     else:
         form = AuthorForm()
-        print('else')
         return render(request, 'records/author_edit.html', {'info': info})
